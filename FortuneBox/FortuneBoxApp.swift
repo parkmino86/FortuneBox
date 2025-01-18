ec36d3a4e7a1daad44387026ec34735e73261831
minoh.park <parkmino86@gmail.com>
2025-01-18 15:39:46 +0900

    Initial Commit

---
+++ FortuneBox/FortuneBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortuneBoxApp.swift
+//  FortuneBox
+//
+//  Created by minoh.park on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FortuneBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FortuneBoxUITests/FortuneBoxUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FortuneBoxUITestsLaunchTests.swift
+//  FortuneBoxUITests
+//
+//  Created by minoh.park on 1/18/25.
+//
+
+import XCTest
+
+final class FortuneBoxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
